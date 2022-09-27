737f6f0d2f0eea89f750f7e07ce57187aadda848
niezhiliang <niezl@hzzh.com>
2022-09-27 10:10:01 +0800

    feat:init

---
+++ README.md
@@ -0,0 +1,5 @@
+# 多环境只打包某一个环境的配置到包中
+
+打包命令指定两个环境
+
+mvn clean install -P env-sjt,test -DskipTests=true
---
+++ src/main/java/com/niezhiliang/App.java
@@ -0,0 +1,9 @@
+package com.niezhiliang;
+
+/**
+ * @author niezhiliang
+ * @version v
+ * @date 2022/9/27 9:58
+ */
+public class App {
+}
